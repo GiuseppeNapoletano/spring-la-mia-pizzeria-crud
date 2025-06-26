3954522667ebe98306320dc272c4fdfc07f0ab5b
GiuseppeNapoletano <andreanap04@gmail.com>
2025-06-26 15:59:07 +0200

    feat:second stage

--- spring-la-mia-pizzeria-crud/src/main/java/org/project/java/pizzeria/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ spring-la-mia-pizzeria-crud/src/main/java/org/project/java/pizzeria/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -1,10 +1,13 @@
 package org.project.java.pizzeria.spring_la_mia_pizzeria_crud.controller;
 
+
+import org.project.java.pizzeria.spring_la_mia_pizzeria_crud.model.Pizza;
 import org.project.java.pizzeria.spring_la_mia_pizzeria_crud.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,5 +21,12 @@ public class PizzaController {
     public String index(Model model){
         model.addAttribute("pizzas", pizzaRepository.findAll());
         return "pizzas/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable Integer id, Model model){
+        Pizza pizza = pizzaRepository.findById(id).get();
+        model.addAttribute("pizza", pizza);
+        return "pizzas/show";
     }
 }
